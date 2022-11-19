53464aef00e7c9a4f70c02d0a89bf68e35386485
eapepe0 <eapepe0@gmail.com>
2022-11-19 15:12:04 -0300

    Capitulo 15 : Proteccion de rutas, useContext, Provider, reducer

---
+++ src/auth/context/AuthProvider.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useReducer } from 'react'
+import { AuthContext } from './AuthContext'
+import { authReducer } from './authReducer'
+
+const initialState = {
+    logged: false,
+}
+export const AuthProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(authReducer, initialState);
+    return (
+        <AuthContext.Provider value={{}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
